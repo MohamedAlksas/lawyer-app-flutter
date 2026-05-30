7af34734bb1a9467db9975db3df883d0d48ea663
MohamedAlksas <adelm8267@gmail.com>
2026-05-30 23:59:24 +0300

    Fix Android build: AGP 8.11.1, Gradle 8.14, Kotlin 2.2.20 for file_picker 11.x compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,13 +33,6 @@ android {
         }
     }
 }
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
-}
-
 flutter {
     source = "../.."
 }
